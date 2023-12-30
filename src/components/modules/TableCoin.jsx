0207355b7accb89ca24c3a7f1cb898995a2aaeff
Jamal Shamsi <93354040+jamalshamsi1988@users.noreply.github.com>
2023-12-30 23:44:14 +0330

    add a seprate component for table row

--- src/components/modules/TableCoin.jsx
+++ src/components/modules/TableCoin.jsx
@@ -1,7 +1,6 @@
-import chartUp from '../../assets/chart-up.svg';
-import chartDown from '../../assets/chart-down.svg';
-const TableCoin = ({coins}) => {
-    console.log(coins)
+import chartUp from "../../assets/chart-up.svg";
+import chartDown from "../../assets/chart-down.svg";
+const TableCoin = ({ coins }) => {
   return (
     <div>
       <table>
@@ -16,24 +15,41 @@ const TableCoin = ({coins}) => {
         </thead>
         <tbody>
           {coins.map((coin) => (
-            <tr key={coin.id}>
-              <td>
-                <div>
-                  <img src={coin.image} alt={coin.name} />
-                  <span>{coin.symbol.toUpperCase()}</span>
-                </div>
-              </td>
-              <td>{coin.name}</td>
-              <td>${coin.current_price.toLocaleString()}</td>
-              <td>{coin.price_change_percentage_24h.toFixed(2)}%</td>
-              <td>{coin.total_volume.toLocaleString()}</td>
-              <td><img src={coin.price_change_percentage_24h > 0 ? chartUp : chartDown} alt={coin.name}/></td>
-            </tr>
+            <TableRow coin={coin} key={coin.id} />
           ))}
         </tbody>
       </table>
     </div>
   );
-}
+};
 
-export default TableCoin
+export default TableCoin;
+
+const TableRow = ({
+  coin: {
+    name,
+    image,
+    symbol,
+    current_price,
+    total_volume,
+    price_change_percentage_24h: price_change,
+  },
+}) => {
+  return (
+    <tr>
+      <td>
+        <div>
+          <img src={image} alt={name} />
+          <span>{symbol.toUpperCase()}</span>
+        </div>
+      </td>
+      <td>{name}</td>
+      <td>${current_price.toLocaleString()}</td>
+      <td>{price_change.toFixed(2)}%</td>
+      <td>{total_volume.toLocaleString()}</td>
+      <td>
+        <img src={price_change > 0 ? chartUp : chartDown} alt={name} />
+      </td>
+    </tr>
+  );
+};
