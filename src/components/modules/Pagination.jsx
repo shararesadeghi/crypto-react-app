e0c68dae26c3d52519d7394784c462a95b36926e
Jamal Shamsi <93354040+jamalshamsi1988@users.noreply.github.com>
2023-12-31 01:11:04 +0330

    pagination styles added to project

--- src/components/modules/Pagination.jsx
+++ src/components/modules/Pagination.jsx
@@ -1,3 +1,4 @@
+import styles from './Pagination.module.css';
 
 const Pagination = ({page, setPage}) => {
   
@@ -11,20 +12,30 @@ const Pagination = ({page, setPage}) => {
     setPage((page) => page + 1);
   };
   return (
-    <div>
-      <button onClick={previousHandler}>previous</button>
-      <p>1</p>
-      <p>2</p>
+    <div className={styles.pagination}>
+      <button
+        onClick={previousHandler}
+        className={page === 1 ? styles.disabled : null}
+      >
+        previous
+      </button>
+      <p className={page === 1 ? styles.selected : null}>1</p>
+      <p className={page === 2 ? styles.selected : null}>2</p>
       {page > 2 && page < 9 && (
         <>
           <span>...</span>
-          <p>{page}</p>
+          <p className={styles.selected}>{page}</p>
         </>
       )}
       <span>...</span>
-      <p>9</p>
-      <p>10</p>
-      <button onClick={nextHandler}>next</button>
+      <p className={page === 9 ? styles.selected : null}>9</p>
+      <p className={page === 10 ? styles.selected : null}>10</p>
+      <button
+        onClick={nextHandler}
+        className={page === 10 ? styles.disabled : null}
+      >
+        next
+      </button>
     </div>
   );
 };
