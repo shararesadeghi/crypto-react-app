91cc646d8842d9665db8c5d67312e16b114771b2
Jamal Shamsi <93354040+jamalshamsi1988@users.noreply.github.com>
2023-12-31 15:56:34 +0330

    some bug handle in search

--- src/components/modules/Search.jsx
+++ src/components/modules/Search.jsx
@@ -51,17 +51,19 @@ const Search = ({ currency, setCurrency }) => {
         <option vlaue="eur">EUR</option>
         <option value="jpy">JPY</option>
       </select>
-      <div className={styles.searchResult}>
-        {isLoading && <RotatingLines strokeWidth="2" strokeColor="#3874ff" />}
-        <ul>
-          {coins.map((coin) => (
-            <li key={coin.id}>
-              <img src={coin.thumb} alt={coin.name} />
-              <p>{coin.name}</p>
-            </li>
-          ))}
-        </ul>
-      </div>
+      {(!!coins.length || isLoading) && (
+        <div className={styles.searchResult}>
+          {isLoading && <RotatingLines strokeWidth="2" strokeColor="#3874ff" />}
+          <ul>
+            {coins.map((coin) => (
+              <li key={coin.id}>
+                <img src={coin.thumb} alt={coin.name} />
+                <p>{coin.name}</p>
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
     </div>
   );
 };
